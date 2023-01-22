7a102eef1411364b8a53ece0c9c0918116a76a3b
jnmarti <88381891+jnmarti@users.noreply.github.com>
2023-01-22 10:49:47 +0000

    add date input

--- news_analysis/app.py
+++ news_analysis/app.py
@@ -7,7 +7,7 @@ import nlplot
 
 import streamlit as st
 
-from news_analysis.utils.dataset_utils import text_to_date, get_dataset
+from news_analysis.utils.dataset_utils import get_dataset
 
 dataset_id = "justinian336/salvadoran-news"
 
@@ -15,9 +15,15 @@ st.markdown("## News Analysis")
 
 if st.session_state.get("df") is None:
     with st.spinner("Loading dataset..."):
-        df = get_dataset(dataset_id)
+        st.session_state["df"] = get_dataset(dataset_id)
+        df = st.session_state["df"]
 else:
     df = st.session_state["df"]
+
+start_date = st.sidebar.date_input(label="Start Date", value=df["date"].min(), max_value=df["date"].max())
+end_date = st.sidebar.date_input(label="Start Date", value=df["date"].max(), min_value=start_date)
+
+df = df[(df["date"] >= start_date) & (df["date"] <= end_date)]
 
 tab1, tab2 = st.tabs(["Dataset Stats", "News Explorer"])
 
--- news_analysis/utils/dataset_utils.py
+++ news_analysis/utils/dataset_utils.py
@@ -1,12 +1,15 @@
 import dateparser
 from datasets import load_dataset
-
-def text_to_date(text):
-    return dateparser.parse(text)
+import pandas as pd
 
 def get_dataset(dataset_id):
     dataset = load_dataset(dataset_id)
     df = dataset["train"].to_pandas()
     df["category_name"] = df["category"].apply(dataset["train"].features["category"].int2str)
-    # df["date"] = df["date"].apply(text_to_date)
+    df["date"] = pd.to_datetime(df["date"]).dt.date
     return df
+
+def filter_by_date(df, start_date, end_date):
+    start_date = text_to_date(start_date)
+    end_date = text_to_date(end_date)
+    return df[(df["date"] >= start_date) & (df["date"] <= end_date)]
